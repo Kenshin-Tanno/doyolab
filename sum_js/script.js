801fbcc4c0f2d893d0296bd7513b558882514721
Kenshin_Tanno <r201901924yp@jindai.jp>
2021-03-27 12:52:18 +0900

    ”20210327”

--- sum_js/script.js
+++ sum_js/script.js
@@ -11,4 +11,52 @@ console.log("画面の高さ",window.innerHeight)
         }
     }
  })
+
+ const targetIntroduction = document.querySelector(".pic");
+ console.log("画面の高さ",window.innerHeight)
+ 
+  document.addEventListener("scroll",function() {
+         const getElementDistance = targetIntroduction.getBoundingClientRect().
+         top + targetIntroduction.clientHeight * .6
+ 
+         if(window.innerHeight > getElementDistance){
+            targetIntroduction.classList.add("show");
+         }
+     }
+  )
+
+  const targetWords = document.querySelector("header");
+ console.log("画面の高さ",window.innerHeight)
+ 
+  document.addEventListener("scroll",function() {
+         const getElementDistance = targetWords.getBoundingClientRect().
+         top + targetWords.clientHeight * .6
+ 
+         if(window.innerHeight > getElementDistance){
+            targetWords.classList.add("show");
+         }
+     }
+  )
+ 
+ const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    speed:1000,
+
+    autoplay:{
+        delay:4000
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
  
