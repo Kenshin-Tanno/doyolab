05022eb6e856633d7cdc03b1bb107bd4056a4cd0
Kenshin_Tanno <r201901924yp@jindai.jp>
2021-03-28 09:06:46 +0900

    ”20210328”

---
+++ sum_js/sec_script.js
@@ -0,0 +1,62 @@
+ const targetElement = document.querySelectorAll(".animationTarget");
+console.log("画面の高さ",window.innerHeight)
+
+ document.addEventListener("scroll",function() {
+    for(let i = 0; i < targetElement.length; i++){
+        const getElementDistance = targetElement[i].getBoundingClientRect().
+        top + targetElement[i].clientHeight * .6
+
+        if(window.innerHeight > getElementDistance){
+            targetElement[i].classList.add("show");
+        }
+    }
+ })
+
+ const targetIntroduction = document.querySelector(".pic");
+ console.log("画面の高さ",window.innerHeight)
+ 
+  document.addEventListener("scroll",function() {
+         const getElementDistance = targetIntroduction.getBoundingClientRect().
+         top + targetIntroduction.clientHeight * .6
+ 
+         if(window.innerHeight > getElementDistance){
+            targetIntroduction.classList.add("show");
+         }
+     }
+  )
+
+  const targetWords = document.querySelector(".big");
+ console.log("画面の高さ",window.innerHeight)
+ 
+  document.addEventListener("scroll",function() {
+         const getElementDistance = targetWords.getBoundingClientRect().
+         top + targetWords.clientHeight * .6
+ 
+         if(window.innerHeight > getElementDistance){
+            targetWords.classList.add("show");
+         }
+     }
+  )
+ 
+ const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    speed:1000,
+
+    autoplay:{
+        delay:4000
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
+ 
